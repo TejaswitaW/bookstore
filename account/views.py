5735a2bf18ed9c2de2bfde2edf0d18e1fd806a10
tejaswitaw <31151862+TejaswitaW@users.noreply.github.com>
2024-12-16 22:06:07 +0530

    User registration view function added

--- account/views.py
+++ account/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+from forms import RegistrationForm
 
 # Create your views here.
+def account_register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        user = registerForm.save(commit=False)
+        user.email = registerForm.cleaned_data['email']
+        user.set_password(registerForm.cleaned_data['password'])
+        # After email verification will make user as active 
+        user.is_active = False
+        user.save()
