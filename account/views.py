a5faea5b0cb3682b7d5a5e575a72b1e4fd1972bc
tejaswitaw <31151862+TejaswitaW@users.noreply.github.com>
2024-12-17 07:19:59 +0530

    account activation token generator class implemented

--- account/views.py
+++ account/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+
+
 
 from forms import RegistrationForm
+from .token import account_activation_token
 
 # Create your views here.
 def account_register(request):
@@ -15,3 +22,13 @@ def account_register(request):
         # After email verification will make user as active 
         user.is_active = False
         user.save()
+        # Setup email
+        current_site = get_current_site(request)
+        subject = 'Activate your account'
+        message = render_to_string('account/registration/account_activation_email.html',{
+            'user': user,
+            'doamin': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': account_activation_token.make_token(user),
+        })
+        user.email_user(subject=subject, message=message)
